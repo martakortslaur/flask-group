c109130c9821d2d90587ea9977243723c8d9cf2f
martakortslaur <martakortslaur@gmail.com>
2019-12-05 11:57:12 +0000

    Adding dictionary

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
 import os
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "This is the home page"
+    return "<h1>This is the home page</h1><br><p>Bonjour!</p>"
 
 @app.route("/europe")
 def europe():
-    return "This is the europe home page"
+    return render_template("index.html")
+
+@app.route("/estonia")
+def estonia():
+    city_01 = {
+        "name" : "Tallinn",
+        "descr" : "Capital"
+    }
+    city_02 = {
+        "name" : "Tartu",
+        "descr" : "University"
+    }
+    cities = [city_01, city_02]
+    return render_template("country.html", cities=cities)
 
 @app.route("/north-america")
 def america():
